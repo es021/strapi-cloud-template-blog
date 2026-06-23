b1b61760f6d08b4ca1e8e65389d8ff18b5f925da
wan <zulsarhan.shaari@gmail.com>
2026-06-23 12:29:04 +0800

    style: format index.js with prettier-style conventions

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 const bootstrap = require("./bootstrap");
 
 module.exports = {
@@ -22,29 +22,47 @@ module.exports = {
     } catch (e) {
       serviceAccountError = e.message;
     }
-    strapi.log.info('[GCS ENV CHECK] GCS_BUCKET_NAME:', bucketName || '(not set)');
-    strapi.log.info('[GCS ENV CHECK] CDN_BASE_URL:', cdnBaseUrl || '(not set)');
-    strapi.log.info('[GCS ENV CHECK] GCS_SERVICE_ACCOUNT set:', !!serviceAccountRaw);
-    strapi.log.info('[GCS ENV CHECK] GCS_SERVICE_ACCOUNT valid JSON:', serviceAccountParsed);
-    if (serviceAccountError) strapi.log.error('[GCS ENV CHECK] GCS_SERVICE_ACCOUNT parse error:', serviceAccountError);
+    strapi.log.info(
+      `[GCS ENV CHECK] GCS_BUCKET_NAME:${bucketName || "(not set)"}`,
+    );
+    strapi.log.info(
+      `[GCS ENV CHECK] CDN_BASE_URL:${cdnBaseUrl || "(not set)"}`,
+    );
+    strapi.log.info(
+      `[GCS ENV CHECK] GCS_SERVICE_ACCOUNT set:${!!serviceAccountRaw}`,
+    );
+    strapi.log.info(
+      `[GCS ENV CHECK] GCS_SERVICE_ACCOUNT valid JSON:${serviceAccountParsed}`,
+    );
+    if (serviceAccountError) {
+      strapi.log.error(
+        `[GCS ENV CHECK] GCS_SERVICE_ACCOUNT parse error:${serviceAccountError}`,
+      );
+    }
 
     strapi.db.lifecycles.subscribe({
-      models: ['plugin::upload.file'],
+      models: ["plugin::upload.file"],
       beforeCreate(event) {
         const { data } = event.params;
         if (data.name) {
-          data.name = data.name.replace(/\s+/g, '_');
+          data.name = data.name.replace(/\s+/g, "_");
         }
         if (data.url) {
           const u = new URL(data.url);
-          u.pathname = u.pathname.split('/').map(s => encodeURIComponent(decodeURIComponent(s))).join('/');
+          u.pathname = u.pathname
+            .split("/")
+            .map((s) => encodeURIComponent(decodeURIComponent(s)))
+            .join("/");
           data.url = u.toString();
         }
         if (data.formats) {
           for (const format of Object.values(data.formats)) {
             if (format.url) {
               const u = new URL(format.url);
-              u.pathname = u.pathname.split('/').map(s => encodeURIComponent(decodeURIComponent(s))).join('/');
+              u.pathname = u.pathname
+                .split("/")
+                .map((s) => encodeURIComponent(decodeURIComponent(s)))
+                .join("/");
               format.url = u.toString();
             }
           }
