dd0b645baf2eeb82aa960420aa3c6973c90e76c7
wan <zulsarhan.shaari@gmail.com>
2026-06-23 12:49:04 +0800

    chore: disable GCP

--- config/plugins.js
+++ config/plugins.js
@@ -1,15 +1,21 @@
-module.exports = ({ env }) => ({
-  upload: {
-    config: {
-      provider: 'strapi-provider-upload-google-cloud-storage',
-      providerOptions: {
-        serviceAccount: env.json('GCS_SERVICE_ACCOUNT'),
-        bucketName: env('GCS_BUCKET_NAME'),
-        publicFiles: false,
-        uniform: true,
-        basePath: '',
-        baseUrl: env('CDN_BASE_URL', `https://storage.googleapis.com/${env('GCS_BUCKET_NAME')}`),
+module.exports = ({ env }) => {
+  const useGCS = env.bool('USE_GCS', false);
+
+  if (!useGCS) return {};
+
+  return {
+    upload: {
+      config: {
+        provider: 'strapi-provider-upload-google-cloud-storage',
+        providerOptions: {
+          serviceAccount: env.json('GCS_SERVICE_ACCOUNT'),
+          bucketName: env('GCS_BUCKET_NAME'),
+          publicFiles: false,
+          uniform: true,
+          basePath: '',
+          baseUrl: env('CDN_BASE_URL', `https://storage.googleapis.com/${env('GCS_BUCKET_NAME')}`),
+        },
       },
     },
-  },
-});
+  };
+};
